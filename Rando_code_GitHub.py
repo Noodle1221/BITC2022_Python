6338c39e87a938b94edf435d4f55a1bd918387c1
Noodle1221 <107993485+Noodle1221@users.noreply.github.com>
2022-07-06 14:24:01 +0200

    Add files via upload

---
+++ Rando_code_GitHub.py
@@ -0,0 +1,11 @@
+## I want to play a game ...
+
+from random import choice
+print("Will you win the coin flip?")
+
+coin_flip = choice(["Heads","Tails"])
+
+if coin_flip == "Heads":
+    print("You win!")
+else:
+    print("You lose!")

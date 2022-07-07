6c081bf377fc3ef02e19748165f40584549a7477
pvanheus <pvh@sanbi.ac.za>
2022-07-07 09:47:16 +0200

    Edited for pep8 compliance
    Checked the code style using an [online pep8 checker](http://pep8online.com/). [PEP8](https://peps.python.org/pep-0008/) is the most commonly used style guide for Python code. You can install tools like `flake8` locally to do these kind of checks.
    
    Also added a ["hashbang"](https://en.wikipedia.org/wiki/Shebang_(Unix)) line. This is one half of turning a script into an executable script. It tells the system where to find the interpreter for the script - in this case using `/usr/bin/env` to find `python` (using the PATH environment variable). This is better than `#!/usr/bin/python` because it will find the Python interpreter that you are currently using (from your conda environment), not the system Python interpreter. 
    
    The second part of making a script executable is giving it executable permissions: `chmod a+x Rando_code_GitHub.py`.

--- Rando_code_GitHub.py
+++ Rando_code_GitHub.py
@@ -1,9 +1,12 @@
-## I want to play a game ...
+#!/usr/bin/env python
+
+# I want to play a game ...
 
 from random import choice
+
 print("Will you win the coin flip?")
 
-coin_flip = choice(["Heads","Tails"])
+coin_flip = choice(["Heads", "Tails"])
 
 if coin_flip == "Heads":
     print("You win!")
